269fed08df75197deef165592b82aa5eb1f84fb5
naoya-ohkita <okita.n@dream-jack.com>
2025-11-10 16:50:21 +0900

    変数名を修正しました

--- yaku_pattern/yaku_1han/tanyao.py
+++ yaku_pattern/yaku_1han/tanyao.py
@@ -2,15 +2,16 @@ YAOCHUHAI_INDICES = ("1p", "9p", "1s", "9s", "1m", "9m",
                     "east", "south", "west", "north", 
                     "white", "green", "red")
 
-def check_tanyao(hands_set) -> bool:
+def check_tanyao(formed_hand, call) -> bool:
     """
     手牌がタンヤオか判定する
     """
-    hand = hands_set["hand"]
-    head = hands_set["head"]
+    hand = formed_hand["hand"]
+    head = formed_hand["head"]
 
     # アタマとそれ以外の手牌を一つのリストにまとめる
     hand.append(head)
+    hand.append(call)
 
     # リスト内に么九牌がないか確認する
     for pair in hand:
@@ -21,5 +22,6 @@ def check_tanyao(hands_set) -> bool:
     return True
 
 hand_set = {"hand":[["1m","2m","3m"],["1m","2m","3m"],["5s","6s","7s"],["6s","7s","8s"]],"head":["8m","8m"]}
+call = []
 
-print(check_tanyao(hand_set))
+print(check_tanyao(hand_set, call))
---
+++ yaku_pattern/yaku_2han/sansyoku_doko.py
@@ -0,0 +1,5 @@
+def check_sansyoku_doko(hand_set):
+    """
+    三色同刻が成立するか判定する
+    """
+    hand = hand_set["hand"]
