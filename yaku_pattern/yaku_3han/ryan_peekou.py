56b2e65a8623133e9c8872340eca95a20ce9459e
naoya-ohkita <okita.n@dream-jack.com>
2025-11-14 13:24:39 +0900

    二盃口判定関数の追加

---
+++ yaku_pattern/yaku_3han/ryan_peekou.py
@@ -0,0 +1,36 @@
+import const
+
+def check_ryan_peekou(menzen, formed_hand) -> str:
+
+    # 面前でなければNoneを返す
+    if not menzen:
+        return None
+    
+    hand = formed_hand["hand"]
+
+    # 手牌の情報を数値に変換する
+    numbered_hand = const.change_hand_to_num(hand)
+
+    # 順子であるか判定し、順子であればリストに格納する
+    shuntsu_list = []
+    for mentsu in numbered_hand:
+        if const.find_shuntsu(mentsu):
+            shuntsu_list.append(mentsu)
+    
+    # 一盃口、二盃口が成立するか判定する
+    for tile in shuntsu_list:
+        # 順序が完全に一致する重複を探す
+        finish_hand = set(tuple(tile) for tile in shuntsu_list)
+    
+    # 一盃口、二盃口であれば、順子の数と重複を除いた順子の数が異なる
+    # 二盃口の判定
+    if len(shuntsu_list) - len(finish_hand) == 2:
+        return "二盃口"
+    # 一盃口の判定
+    elif len(shuntsu_list) - len(finish_hand) == 1:
+        return "一盃口"
+    else:
+        return None
+
+
+
