21fa468367b0c362c158715a418f2b6270da7356
K-Ohisama0315 <ishida.m@dream-jack.com>
2025-11-18 16:48:28 +0900

    清老頭と混老頭をまとめました

--- yaku_pattern/yakuman/chin_routou.py
+++ yaku_pattern/yakuman/chin_routou.py
@@ -9,10 +9,10 @@ def check_chin_routou(situation_input):
 
     # 中張牌が含まれる場合False
     if (not const.find_full_yaochu(tiles_index)):
-        return False
+        return None
     
     # 字牌が含まれる場合False
     if any(index > 30 for index in tiles_index):
-        return False
+        return "混老頭"
     
-    return True
+    return "清老頭"
