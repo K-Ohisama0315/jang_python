7897aeec812e968705bb1451561a39e1e4f35359
K-Ohisama0315 <ishida.m@dream-jack.com>
2025-11-19 14:36:28 +0900

    init書き換え

--- yaku_pattern/__init__.py
+++ yaku_pattern/__init__.py
@@ -7,6 +7,7 @@ from .yaku_2han.chiitoitsu import check_chiitoitsu
 from .yaku_2han.ikkitsuukan import check_ikkitsuukann
 from .yaku_2han.sansyoku_dojun import check_sansyoku_dojun
 from .yaku_2han.sansyoku_doko import check_sansyoku_doko
+from .yaku_2han.toitoi_ho import check_toitoi_ho
 
 # 三翻
 from .yaku_3han.jun_chanta import check_jun_chanta
@@ -16,10 +17,15 @@ from .yaku_3han.ryan_peekou import check_ryan_peekou
 from .yaku_6han.chin_iiso import check_chin_iiso
 
 # 役満
-from .yakuman.kokushi_musou import check_kokushi_musou
-from .yakuman.chuuren_poutou import check_chuuren
-from .yakuman.ryu_iiso_tsu_iiso import check_ryu_iiso_tsu_iiso
 from .yakuman.chin_routou import check_chin_routou
+from .yakuman.chuuren_poutou import check_chuuren
+from .yakuman.daisangen import check_daisangen
+from .yakuman.daisuushi import check_daisuushi
+from .yakuman.kokushi_musou import check_kokushi_musou
+from .yakuman.ryu_iiso_tsu_iiso import check_ryu_iiso_tsu_iiso
+from .yakuman.su_anko import check_su_anko
+from .yakuman.su_kantsu import check_su_kantsu
+
 
 # initファイルで外部に公開する関数を定義
 __all__ = [
@@ -30,15 +36,19 @@ __all__ = [
     "check_ikkitsuukann",
     "check_sansyoku_dojun",
     "check_sansyoku_doko",
+    "check_toitoi_ho",
 
     "check_jun_chanta",
     "check_ryan_peekou",
 
     "check_chin_iiso",
 
-    "check_kokushi_musou",
+    "check_chin_routou",
     "check_chuuren",
+    "check_daisangen",
+    "check_daisuushi",
+    "check_kokushi_musou",
     "check_ryu_iiso_tsu_iiso",
-    "check_chin_routou"
-
+    "check_su_anko",
+    "check_su_kantsu"
 ]
