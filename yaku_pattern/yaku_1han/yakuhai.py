83a7b4ae9924896be22e1d5a371f71d08861572f
K-Ohisama0315 <ishida.m@dream-jack.com>
2025-11-21 10:51:25 +0900

    役牌が集合を戻り値にするように修正

--- yaku_pattern/yaku_1han/yakuhai.py
+++ yaku_pattern/yaku_1han/yakuhai.py
@@ -1,29 +1,35 @@
-yaku_hai = {"white":"white", "green":"green", "red":"red", "jikaze":"", "bakaze":""}
+yaku_hai = {"役牌：白":"white", "役牌：發":"green", "役牌：中":"red", "役牌：自風牌":"", "役牌：場風牌":""}
 
 # 役牌判定関数
 def check_yaku_hai(jikaze, bakaze, formed_hand, formed_call) -> dict:
 
     # 役牌の辞書に自風、場風を追加する
-    yaku_hai["jikaze"] = jikaze
-    yaku_hai["bakaze"] = bakaze
+    yaku_hai["役牌：自風牌"] = jikaze
+    yaku_hai["役牌：場風牌"] = bakaze
 
     hand = formed_hand["hand"].copy()
 
-    yaku_dict = {"white":False, "green":False, "red":False, "jikaze":False, "bakaze":False}
+    yakuhai_set = set()
     
     # 副露牌を手牌に追加
     for mentsu in formed_call:
         hand.append(mentsu)
 
-    # 役ごとに刻子ができているか判定する
-    for mentsu in hand:
-        for key, yaku in yaku_hai.items():
-            count = 0
-            for tile in mentsu:
-                if tile == yaku:
-                    count += 1
+    # 面子の先頭を追加(役牌は刻子、槓子にしかならないため)
+    target = {mentsu[0] for mentsu in hand}
+
+    # マッチするものを集合に格納
+    yakuhai_set = {k for k, v in yaku_hai.items if v in target}
+
+    # # 役ごとに刻子ができているか判定する
+    # for mentsu in hand:
+    #     for key, yaku in yaku_hai.items():
+    #         count = 0
+    #         for tile in mentsu:
+    #             if tile == yaku:
+    #                 count += 1
                 
-            if count >= 3:
-                yaku_dict[key] = True
+    #         if count >= 3:
+    #             yaku_dict[key] = True
     
-    return yaku_dict
+    return yakuhai_set
